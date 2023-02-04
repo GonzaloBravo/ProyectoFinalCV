81a4e0923749bb0df1abe7ccb979a4acbc628d2b
GonzaloBravo <gonzalobravo222@gmail.com>
2023-02-04 18:15:59 -0300

    Proyecto Final Integrador - Argentina Programa 4.0

---
+++ js/function.js
@@ -0,0 +1,8 @@
+
+//Boton que al hacer click reemplaza el texto y muestra un listado//
+document.getElementById('boton').onclick = function name() {
+    console.log("Capturamos el evento click")
+    document.getElementById("redes").innerHTML = "facebook.com/gonzalobravo - instagram.com/gonzalobravo - linkedin.com/gonzalobravo"
+}
+
+
